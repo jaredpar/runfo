dc9f83bbed8e442c39448386b7939016e2f7ea22
Jared Parsons <jaredpparsons@gmail.com>
2020-04-13 09:54:44 -0700

    Initial status page work
    Ports the status page work from the scratch project that I'd been
    working on. Now feel a bit more confident that I want to spend more time
    pushing on this and making it at least a usable side project.

--- DevOps.Status/Pages/Index.cshtml.cs
+++ DevOps.Status/Pages/Index.cshtml.cs
@@ -4,22 +4,53 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Octokit;
 
 namespace DevOps.Status.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly IConfiguration _configuration;
         private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public List<Issue> BlockingOfficial { get; set; }
+
+        public List<Issue> BlockingNormal { get; set; }
+        public List<Issue> BlockingNormalOptional { get; set; }
+        public List<Issue> BlockingOuterloop { get; set; }
+
+        public IndexModel(IConfiguration configuration, ILogger<IndexModel> logger)
         {
+            _configuration = configuration;
             _logger = logger;
         }
 
-        public void OnGet()
+        public async Task OnGet()
         {
+            // so much hack
+            var token = _configuration["GitHub:Token"];
+            var gitHub = new GitHubClient(new ProductHeaderValue("RuntimeStatusPage"));
+            gitHub.Credentials = new Credentials("jaredpar", token);
 
+            BlockingOfficial = await DoSearch(gitHub, "blocking-official-build");
+            BlockingNormal = await DoSearch(gitHub, "blocking-clean-ci");
+            BlockingNormalOptional = await DoSearch(gitHub, "blocking-clean-ci-optional");
+            BlockingOuterloop = await DoSearch(gitHub, "blocking-outerloop");
+
+            static async Task<List<Issue>> DoSearch(GitHubClient gitHub, string label)
+            {
+                var request = new SearchIssuesRequest()
+                {
+                    Labels = new [] { label },
+                    State = ItemState.Open,
+                    Type = IssueTypeQualifier.Issue,
+                    Repos = { { "dotnet", "runtime" } }
+                };
+                var result = await gitHub.Search.SearchIssues(request);
+                return result.Items.ToList();
+            }
         }
     }
 }
