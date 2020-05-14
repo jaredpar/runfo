b1a74711b6f19290c1b260f314e023f4e492b3c7
Jared Parsons <jaredpparsons@gmail.com>
2020-05-14 06:28:32 -0700

    Fixed the build query

--- DevOps.Status/wwwroot/js/builds.js
+++ DevOps.Status/wwwroot/js/builds.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {  
     $('#search-button').click(function(e) {
         e.preventDefault();
+        var button = $(this)
+        button.prop('disabled', true);
         var query = $('#search-text').val();
         $.ajax({  
             type: "GET",  
@@ -10,19 +12,29 @@ $(document).ready(function () {
             success: function (data) {  
                 var body = $('#table-body');
                 body.empty();
+
+                var total = 0;
                 $.each(data, function (i, item) {  
                     var row = '<tr>' +
                         '<td>' + item.result + '</td>' +
                         '<td><a href="' + item.buildUri + '">' + item.buildNumber + '</td>' +
                         '<tr>';
                     body.append(row);
+                    total++;
                 });
+
+                var summary = $('#search-summary');
+                summary.empty();
+                summary.append(total + " results returned");
+                button.prop('disabled', false);
             },
             failure: function (data) {  
                 alert(data.responseText);  
+                button.prop('disabled', false);
             },
             error: function (data) {  
                 alert(data.responseText);  
+                button.prop('disabled', false);
             }
         });
     });  
