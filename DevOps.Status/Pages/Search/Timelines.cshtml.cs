20dba3c461543a0e618651306d3ca7d433fdb67c
Jared Parsons <jaredpparsons@gmail.com>
2020-08-04 08:24:31 -0700

    Temp

---
+++ DevOps.Status/Pages/Search/Timelines.cshtml.cs
@@ -0,0 +1,63 @@
+﻿#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DevOps.Status.Util;
+using DevOps.Util.Triage;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DevOps.Status.Pages.Search
+{
+    public class TimelinesModel : PageModel
+    {
+        public class TimelineData
+        {
+            public int BuildNumber { get; set; }
+            public string? BuildUri { get; set; }
+            public string? Message { get; set; }
+            public int Attempt { get; set; }
+            public string? Kind { get; set; }
+        }
+
+        [BindProperty(SupportsGet = true, Name = "bq")]
+        public string? BuildQuery { get; set; }
+
+        [BindProperty(SupportsGet = true, Name = "tq")]
+        public string? TimelineQuery { get; set; }
+
+        public List<TimelineData> TimelineDataList { get; set; } = new List<TimelineData>();
+
+        public TriageContextUtil TriageContextUtil { get; set; }
+
+        public TimelinesModel(TriageContextUtil triageContextUtil)
+        {
+            TriageContextUtil = triageContextUtil;
+        }
+
+        public async Task<IActionResult> OnGet()
+        {
+            if (string.IsNullOrEmpty(BuildQuery) || string.IsNullOrEmpty(TimelineQuery))
+            {
+                if (string.IsNullOrEmpty(BuildQuery))
+                {
+                    BuildQuery = new StatusBuildSearchOptions() { Repository = "runtime" }.GetUserQueryString();
+                }
+
+                if (string.IsNullOrEmpty(TimelineQuery))
+                {
+                    TimelineQuery = "error";
+                }
+
+                return Page();
+            }
+
+            var buildSearchOptions = new StatusBuildSearchOptions();
+            buildOptions.Parse(BuildQuery);
+
+            var timelineSearchOptions = new StatusTimelineSearchOptions();
+        }
+    }
+}
---
+++ DevOps.Status/Util/StatusTimelineSearchOptions.cs
@@ -0,0 +1,60 @@
+﻿#nullable enable
+
+using DevOps.Util.DotNet;
+using DevOps.Util.Triage;
+using Octokit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevOps.Status.Util
+{
+    public class StatusTimelineSearchOptions
+    {
+        public string? Value { get; set; }
+
+        public string GetUserQueryString()
+        {
+            var builder = new StringBuilder();
+            if (!string.IsNullOrEmpty(Value))
+            {
+                Append($"value:\"{Value}\"");
+            }
+
+            return builder.ToString();
+
+            void Append(string message)
+            {
+                if (builder.Length != 0)
+                {
+                    builder.Append(" ");
+                }
+
+                builder.Append(message);
+            }
+        }
+
+        public void Parse(string userQuery)
+        {
+            if (!userQuery.Contains(":"))
+            {
+                Value = userQuery.Trim('"');
+                return;
+            }
+
+            foreach (var tuple in DotNetQueryUtil.TokenizeQueryPairs(userQuery))
+            {
+                switch (tuple.Name.ToLower())
+                {
+                    case "value":
+                        Value = tuple.Value.Trim('"');
+                        break;
+                    default:
+                        throw new Exception($"Invalid option {tuple.Name}");
+                }
+            }
+        }
+    }
+}
