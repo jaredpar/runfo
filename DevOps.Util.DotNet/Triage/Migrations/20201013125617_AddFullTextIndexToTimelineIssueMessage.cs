8f8f2b43e5f4d4a4bbd96fa9c69ec9bad74300e5
Jared Parsons <jaredpparsons@gmail.com>
2020-10-13 06:13:47 -0700

    Full text index for messages

---
+++ DevOps.Util.DotNet/Triage/Migrations/20201013125617_AddFullTextIndexToTimelineIssueMessage.cs
@@ -0,0 +1,30 @@
+﻿using Azure.Storage.Queues.Models;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DevOps.Util.DotNet.Triage.Migrations
+{
+    public partial class AddFullTextIndexToTimelineIssueMessage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                sql: "CREATE FULLTEXT CATALOG ftMessage AS DEFAULT;",
+                suppressTransaction: true);
+
+            migrationBuilder.Sql(
+                sql: "CREATE FULLTEXT INDEX ON ModelTimelineIssues(Message) KEY INDEX PK_ModelTimelineIssues;",
+                suppressTransaction: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                sql: "DROP FULLTEXT INDEX ON ModelTimelineIssues;",
+                suppressTransaction: true);
+
+            migrationBuilder.Sql(
+                sql: "DROP FULLTEXT CATALOG ftMessage",
+                suppressTransaction: true);
+        }
+    }
+}
