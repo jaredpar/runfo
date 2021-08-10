71fdee9fa057dc45ec2173eefd8457a9ee9d961b
Jared Parsons <jaredpparsons@gmail.com>
2021-08-10 16:59:03 -0700

    Full text helix content

---
+++ DevOps.Util.DotNet/Triage/Migrations/20210810235630_HelixLogsFullTextIndex.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DevOps.Util.DotNet.Triage.Migrations
+{
+    public partial class HelixLogsFullTextIndex : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                sql: "CREATE FULLTEXT CATALOG ftHelixContent",
+                suppressTransaction: true);
+
+            migrationBuilder.Sql(
+                sql: "CREATE FULLTEXT INDEX ON ModelHelixLogs(Content) KEY INDEX PK_ModelHelixLogs ON ftHelixContent;",
+                suppressTransaction: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                sql: "DROP FULLTEXT INDEX ON ModelHelixLogs;",
+                suppressTransaction: true);
+
+            migrationBuilder.Sql(
+                sql: "DROP FULLTEXT CATALOG ftHelixContent",
+                suppressTransaction: true);
+        }
+    }
+}
